68d24f0deae3fee85285cbb29632640d6e310a0c
baqerali313 <137217410+baqerali313@users.noreply.github.com>
2023-09-11 17:12:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import os
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String, orm
-
-root_path = os.path.dirname(os.path.realpath(__file__))
-db_uri = os.path.join(root_path, 'db.db')
-engine = create_engine(f'sqlite:///{db_uri}', echo=True)
-
-
-base = sqlalchemy.orm.declarative_base()
-base.metadata.create_all(bind=engine)
-class users(base):
-    __tablename__ = 'users'
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(100), nullable=False)
-
 
